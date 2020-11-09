577638c88fd492e4c9b58ffac2a687b6e7bd31ea
Remi Douah <remidouah@gmail.com>
2020-11-08 23:15:26 -0600

    Completed

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius (double fahrenheit)
+        {
+            var result = (fahrenheit - 32 / 1.8);
+            return result;
+
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
